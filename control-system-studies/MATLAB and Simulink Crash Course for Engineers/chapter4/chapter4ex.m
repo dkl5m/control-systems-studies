df69ae077a77b64dfc96f20a297de50467e0e0c3
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-04-03 23:49:12 -0300

    Add example 4.5

--- control-system-studies/MATLAB and Simulink Crash Course for Engineers/chapter4/chapter4ex.m
+++ control-system-studies/MATLAB and Simulink Crash Course for Engineers/chapter4/chapter4ex.m
@@ -53,7 +53,28 @@ disp(['C1*C2 in exponential form: ', num2str(M_mag), 'exp(i*', ...
 D_mag = C1_mag/C2_mag;
 D_angle = C1_angle - C2_angle;
 disp(['C1/C2 in exponential form: ', num2str(D_mag), 'exp(i*', ...
-    num2str(D_angle), ')']);
+    num2str( _angle), ')']);
 
 %%
 % Example 4.4: Eulers Series for Solving Initial Value Problem
+
+%%
+% Example 4.5: Fourier Series
+% input: square wave function
+% T = 2; Magnitude = 1; Omega = pi
+t = -7:0.01:7;
+x = 1 - square(pi*(t + 1));
+plot(t, x, 'LineWidth', 1.5)
+grid on; hold on;
+
+% Fourier Series
+syms k t
+omega = pi;
+T = 2;
+k = 1:5;
+a_0 = (2/T)*int(2, t, 0, 1);
+a_k = (2/T)*int(2*cos(k*omega*t), t, 0, 1);
+b_k = (2/T)*int(2*sin(k*omega*t), t, 0, 1);
+f = (a_0/2) + sum(a_k.*cos(k*omega*t)) + sum(b_k.*sin(k*omega*t));
+fplot(f, [0, 7])
+grid on;
