16aef82b3a28eb85697d2fb177e62226d5553c2f
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-04-11 23:22:55 -0300

    Add example 4.7

--- control-system-studies/MATLAB and Simulink Crash Course for Engineers/chapter4/chapter4ex.m
+++ control-system-studies/MATLAB and Simulink Crash Course for Engineers/chapter4/chapter4ex.m
@@ -90,6 +90,34 @@ disp('Inverse Fourier transform of F: ');
 inv_F
 
 %%
+% Example 4.7: Taylor Series
+clc; clear;
+% Taylor series expansion
+% Function: f(x)=2*sin(x) at a point x=a=0.5;
+% Taylor series expansion yp to 4th and 10th order
+
+syms x;
+f = 2*sin(x);
+a = 0.5;
+T_4 = taylor(f,x,a,'order',4);
+T_10 = taylor(f,x,a,'order',10);
+disp('Taylor series expansion of 2*sin(x) at a=0.5 up to 4th order:');
+T_4
+fplot(T_4,'b','Linewidth',1.5);
+hold on;
+fplot(T_10,'g','Linewidth',1.5);
+hold on;
+fplot(f,'r','Linewidth',1.5);
+hold off;
+xlim([-4 4]);
+ylim([-4 2]);
+grid on
+legend('Taylor series up to 4th order',...
+        'Taylor series up to 10th order',...
+        'Original function: 2*sin(x)','Location','Best')
+title('Taylor series of 2*sin(x) at a = 0.5');
+
+%%
 % Example 4.4: Eulers Series for Solving Initial Value Problem
 
 clc; clear;
