1c9027d60d2c6578006672dafdb629c4ad84ff84
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-04-17 22:34:30 -0300

    Add more chapter 5 examples

--- control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter5 - Copy/chapter5ex.m
+++ control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter5 - Copy/chapter5ex.m
@@ -108,39 +108,3 @@ pie(x,explode);
 title('With explode feature');
 labels = {};
 legend(labels,'Location','best');
-
-%%
-% Example 5.9: Heat Map
-clc; clear; close all;
-
-% Heat Map
-% Data: Electricity day-ahead market price (2019)
-% LMP: Local Marginal Price ($/MW)
-% Date: Jan-01-2019 to Jan-07-2019
-% Time: 10AM to 3PM
-
-LMP = [38.8 38.5 39.6 42.8 42.8 40.8; ...
-       42.2 42.4 43.4 45.0 58.0 55.3; ...
-       40.4 39.6 41.4 44.0 56.9 50.9; ...
-       38.8 39.5 40.0 43.6 55.3 48.5; ...
-       36.6 36.5 36.5 38.2 39.4 39.6; ...
-       32.7 33.1 34.5 35.0 36.5 34.9; ...
-       32.3 33.5 35.1 39.4 48.2 56.0];
-TIme = {'10 AM','10 AM','10 AM','10 AM','10 AM','10 AM',};
-Date = {'Jan-01','Jan-01','Jan-01','Jan-01','Jan-01','Jan-01','Jan-01',};
-H = heatmap(Time, Date, LMP);
-H.Title = 'Electricity day-ahead market price ($/MW)';
-H.XLabel = 'Time';
-H.YLabel = 'Date';
-
-%%
-% Example 5.11: 3D Pie Plots
-clc; clear; close all;
-
-% 3D pie plot
-% Data: Electricity consumption by different sectors in USA (2018)
-x = [35.4,25.9,2,38.5];
-explode = [0,0,1,0];
-pie(x, explode);
-labels = {'Commercial','Industrial','Transportation','Residencial'};
-legend(labels, 'Location', 'best');
