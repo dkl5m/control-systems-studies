efd0046a4c9fc04c01549bc5679b838ea270b787
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-04-30 23:35:51 -0300

    Add ex8.3 and ex8.4

--- control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter8/chapter8ex.m
+++ control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter8/chapter8ex.m
@@ -18,3 +18,33 @@ grid on;
 R1=10; R2=5; R3=4;
 Equivalent_R= R1+((R2*R3)/(R2+R3));
 fprintf('Equivalent resistance: %f', Equivalent_R);
+
+%%
+% Example 8.3: Delta to Wye Conversion
+% Delta configured resistances:
+% Rd1=10 ohms, Rd2=5 ohms, Rd3=20 ohms
+% Equivalent wye configured resistances:
+% Ry1, Ry2, Ry3
+
+clc; clear;
+Rd1=10; Rd2=5; Rd3=20;
+Ry1= (Rd1*Rd2)/(Rd1+Rd2+Rd3);
+Ry2= (Rd1*Rd3)/(Rd1+Rd2+Rd3);
+Ry3= (Rd3*Rd2)/(Rd1+Rd2+Rd3);
+fprint('Equivalent wye configured resistances:\n');
+fprint('Ry1= %f  Ry2= %f  Ry3= %f\n',Ry1,Ry2,Ry3);
+
+%%
+% Example 8.4: Wye to Delta Conversion
+% Delta configured resistances:
+% Rd1=10 ohms, Rd2=5 ohms, Rd3=20 ohms
+% Equivalent wye configured resistances:
+% Ry1, Ry2, Ry3
+
+clc; clear;
+Ry1=10; Ry2=5; Ry3=20;
+Rd1= (Ry1*Ry2+Ry2*Ry3+Ry3*Ry1)/(Ry3);
+Rd2= (Ry1*Ry2+Ry2*Ry3+Ry3*Ry1)/(Ry2);
+Rd3= (Ry1*Ry2+Ry2*Ry3+Ry3*Ry1)/(Ry1);
+fprint('Equivalent delta configured resistances:\n');
+fprint('Rd1= %f  Rd2= %f  Rd3= %f\n',Rd1,Rd2,Rd3);
