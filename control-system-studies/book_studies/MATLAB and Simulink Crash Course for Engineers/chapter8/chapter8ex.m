de3d276dce1f2cef62c69c1dd47a4ddaf15e3c5e
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-05-20 23:11:36 -0300

    Add 8.6 example

--- control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter8/chapter8ex.m
+++ control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter8/chapter8ex.m
@@ -59,7 +59,7 @@ fprint('Rd1= %f  Rd2= %f  Rd3= %f\n',Rd1,Rd2,Rd3);
 % Third step: Find Rp
 % Fourth step: Find overall equivalent resistance, Req
 clc; close; clear;
-R1=2; R2=4; R3=6; R4=3; R5=2;
+Rd1=2; Rd2=4; Rd3=6; R4=3; R5=2;
 Ry1= (Rd1*Rd2)/(Rd1+Rd2+Rd3);
 Ry2= (Rd1*Rd3)/(Rd1+Rd2+Rd3);
 Ry3= (Rd3*Rd2)/(Rd1+Rd2+Rd3);
@@ -68,3 +68,33 @@ Rs2= Ry3+R5;
 Rp= (Rs1*Rs2)/(Rs1+Rs2);
 Req= Ry1+Rp;
 fprintf('The equivalent resistance: %.3f ohms\n', Req);
+
+%%
+% Example 8.6: Circuit Problem
+% R1=2 ohms; R2=R3=4 ohms
+% Voltage source, E=10V
+% Determine loop current IL1 and IL2
+% Determine current, I1, I2, and I3
+% Determine voltage across resistance R1: VR1
+% Determine voltage across resistance R2: VR2
+% Determine voltage across resistance R3: VR3
+clc;clear;
+R1=2;R2=4;R3=R2;
+syms IL1 IL2
+eqn1= 6*IL1-4*IL2==10;
+eqn2= -4*IL1+8*IL2==0;
+[IL1,IL2]=solve(eqn1,eqn2);
+fprintf('The ABC loop current, IL1: %.3f A\n',IL1);
+fprintf('The ABC loop current, IL2: %.3f A\n',IL2);
+
+I1=IL1; I2=IL2; I3=I1-I2;
+fprintf('The currents in the circuit:\n');
+fprintf('I1= %.3f A I2= %.3f A I3= %.3f A\n',I1,I2,I3);
+
+VR1=I1*R1;
+VR2=I3*R2;
+VR3=I2*R3;
+
+fprintf('The voltage across R1, VR1= %.3f V\n',VR1);
+fprintf('The voltage across R2, VR2= %.3f V\n',VR2);
+fprintf('The voltage across R3, VR3= %.3f V\n',VR3);
