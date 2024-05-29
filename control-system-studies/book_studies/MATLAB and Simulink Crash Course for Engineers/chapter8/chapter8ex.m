b1f5ecc3a1538c2fb1d99c41ca4595951a8aeb64
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-05-28 23:11:17 -0300

    Add example 8.12

--- control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter8/chapter8ex.m
+++ control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter8/chapter8ex.m
@@ -189,4 +189,24 @@ fprintf('RMS voltage: %.3f\n', V_rms);
 fprintf('Average voltage: %.3f\n', V_avg);
 fprintf('Instantaneous voltage at T=0.02 sec: %.3f\n', V_inst);
 
-%%
+%% Example 8.12: Impedance
+% R= 10 ohms; L= 0.02 H; C= 0.05 F; f= 60Hz
+% Determine impedance: Z
+% Power factor: PF
+clc;clear;
+
+R= 10; L= 0.02; C= 0.05;
+f=60;
+XL=2*pi*f*L;
+XC=1/(2*pi*f*C);
+disp('Impedance:')
+
+Z=R+1i*(XL-XC);
+Imp_magnitude=abs(Z);
+Phase_angle=angle(Z)*(180/pi);
+disp('In polar form:')
+
+fprintf('|Z|=%.3f ohms; Phase angle= %.3f degree\n',...
+    Imp_magnitude, Phase_angle);
+PF=cos(Phase_angle);
+fprintf('Power factor: %.3f\n', PF);
