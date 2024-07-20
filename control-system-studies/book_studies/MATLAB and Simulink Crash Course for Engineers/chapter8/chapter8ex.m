5a87b42a3cf90889aa9c4d2b5ceb8765a7a520a4
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-07-20 18:08:53 -0300

    Add 8.18 and 8.19

--- control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter8/chapter8ex.m
+++ control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter8/chapter8ex.m
@@ -480,3 +480,102 @@ fprintf('Line currents:\n');
 fprintf('I_A=%.3f A    Angle=%.3f degree\n',I_AN_mag,I_AN_ang);
 fprintf('I_B=%.3f A    Angle=%.3f degree\n',I_BN_mag,I_BN_ang);
 fprintf('I_C=%.3f A    Angle=%.3f degree\n',I_CN_mag,I_CN_ang);
+
+%%
+% Example 8.18: Wye connected three wire unbalanced load
+% Phase voltages:
+% V_AO=120V angle 10deg; V_BO=110V angle 150deg; V_CO=150V angle -50deg;
+% Impedances:
+% Z1=10 Ohms angle 10deg; Z2=15 Ohms angle -25deg; Z3=20 Ohms angle -10deg; 
+% Find: Line to line voltages: V_AB; V_BC; V_CA;
+% Find: Line currents I_A; I_B; I_C;
+clc;clear;
+
+% Line to line voltages
+V_AO=120*cos(10)+i*120*sin(0);
+V_BO=110*cos(150*(pi/180))+i*110*sin(150*(pi/180));
+V_CO=150*cos(-50*(pi/180))+i*150*sin(-50*(pi/180));
+
+% Impedances
+Z1=10*cos(10*(pi/180))+i*10*sin((pi/180));
+Z2=15*cos(-25*(pi/180))+i*15*sin(-25*(pi/180));
+Z3=20*cos(-10*(pi/180))+i*20*sin(-10*(pi/180));
+
+% Line to line voltages
+V_AB=V_AO-V_BO;
+V_BC=V_BO-V_CO;
+V_CA=V_CO-V_AO;
+V_AB_mag=abs(V_AB);
+V_AB_ang=angle(V_AB)*(180/pi);
+V_BC_mag=abs(V_BC);
+V_BC_ang=angle(V_BC)*(180/pi);
+V_CA_mag=abs(V_CA);
+V_CA_ang=angle(V_CA)*(180/pi);
+fprintf('Line to line voltages:\n');
+fprintf('V_AB=%.3f A    Angle=%.3f degree\n',V_AB_mag,V_AB_ang);
+fprintf('V_BC=%.3f A    Angle=%.3f degree\n',V_BC_mag,V_BC_ang);
+fprintf('V_CA=%.3f A    Angle=%.3f degree\n',V_CA_mag,V_CA_ang);
+
+% Line currents
+I_A=V_AO/Z1;
+I_B=V_BO/Z2;
+I_C=V_CO/Z3;
+I_A_mag=abs(I_A);
+I_A_ang=angle(I_A)*(180/pi);
+I_B_mag=abs(I_B);
+I_B_ang=angle(I_B)*(180/pi);
+I_C_mag=abs(I_C);
+I_C_ang=angle(I_C)*(180/pi);
+fprintf('Line currents:\n');
+fprintf('I_A=%.3f A    Angle=%.3f degree\n',I_A_mag,I_A_ang);
+fprintf('I_B=%.3f A    Angle=%.3f degree\n',I_B_mag,I_B_ang);
+fprintf('I_C=%.3f A    Angle=%.3f degree\n',I_C_mag,I_C_ang);
+
+%%
+% Example 8.19: Wye connected three wire balanced load
+% Phase voltages:
+% V_AN=120V angle 0deg; V_BN=120V angle 120deg; V_CN=120V angle 240deg;
+% Impedances:
+% Z=10 Ohms angle 10deg;
+% Find: Line to line voltages: V_AB; V_BC; V_CA;
+% Find: Line currents I_A; I_B; I_C;
+clc;clear;
+
+% Line to line voltages
+V_AO=120*cos(10)+i*120*sin(0);
+V_BO=120*cos(120*(pi/180))+i*120*sin(120*(pi/180));
+V_CO=120*cos(240*(pi/180))+i*120*sin(240*(pi/180));
+
+% Impedances
+Z=10*cos(10*(pi/180))+i*10*sin((pi/180));
+
+% Line to line voltages
+V_AB=V_AO-V_BO;
+V_BC=V_BO-V_CO;
+V_CA=V_CO-V_AO;
+V_AB_mag=abs(V_AB);
+V_AB_ang=angle(V_AB)*(180/pi);
+V_BC_mag=abs(V_BC);
+V_BC_ang=angle(V_BC)*(180/pi);
+V_CA_mag=abs(V_CA);
+V_CA_ang=angle(V_CA)*(180/pi);
+fprintf('Line to line voltages:\n');
+fprintf('V_AB=%.3f A    Angle=%.3f degree\n',V_AB_mag,V_AB_ang);
+fprintf('V_BC=%.3f A    Angle=%.3f degree\n',V_BC_mag,V_BC_ang);
+fprintf('V_CA=%.3f A    Angle=%.3f degree\n',V_CA_mag,V_CA_ang);
+
+% Line currents
+I_A=V_AO/Z;
+I_B=V_BO/Z;
+I_C=V_CO/Z;
+I_A_mag=abs(I_A);
+I_A_ang=angle(I_A)*(180/pi);
+I_B_mag=abs(I_B);
+I_B_ang=angle(I_B)*(180/pi);
+I_C_mag=abs(I_C);
+I_C_ang=angle(I_C)*(180/pi);
+fprintf('Line currents:\n');
+fprintf('I_A=%.3f A    Angle=%.3f degree\n',I_A_mag,I_A_ang);
+fprintf('I_B=%.3f A    Angle=%.3f degree\n',I_B_mag,I_B_ang);
+fprintf('I_C=%.3f A    Angle=%.3f degree\n',I_C_mag,I_C_ang);
+
