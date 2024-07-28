044b8c3fae523ad2cab4abc4b20fb17b0bcd0f53
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-07-27 22:59:07 -0300

    add 8.23

--- control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter8/chapter8ex.m
+++ control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter8/chapter8ex.m
@@ -629,3 +629,26 @@ I_R=-v_out/R;
 I_C=limit(C*diff(v,t),t,0.1);
 fprintf('I_F at t=0.1 sec: %.5f A\n\n', I_F);
 fprintf('I_C at t=0.1 sec: %.5f A\n\n', I_C);
+
+%%
+% Example 8.23: Integrator circuit
+% R=5 Ohms; C=0.5 F;
+% Input signal: v(t)=2sin(t);
+% Find: Output signal, v_out(t);
+% Find: Output at t=0.1 sec;
+% Find: I_R and I_C at t=0.1 sec;
+clc;clear;
+
+R=5;C=0.5;
+syms t
+v=@(t) 2*sin(t);
+v_out=@(t) (-1/R*C)*int(v,t);
+fprintf('The output signal:\n');
+disp(v_out(t))
+v_out=limit(v_out,t,0.1);
+fprintf('The output voltage at t=0.1 sec: %.5f V\n\n', v_out);
+
+I_R=-v_out/R;
+I_C=limit(C*diff(v,t),t,0.1);
+fprintf('I_R at t=0.1 sec: %.5f A\n\n', I_R);
+fprintf('I_C at t=0.1 sec: %.5f A\n\n', I_C);
