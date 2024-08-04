d0114b9c03b82747dd9fa844c2902de8b576838b
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-08-03 23:17:29 -0300

    add 9.2 and 9.3

--- control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter9/chapter9ex.m
+++ control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter9/chapter9ex.m
@@ -10,3 +10,28 @@ G1=@(s)(s+50)/(s^2+11*s+12);
 disp('Transfer function:');
 disp(G1(s))
 
+%%
+% Example 9.2. Laplace Transform
+clc;clear;
+
+syms t s
+g=@(t) exp(3*t)*sin(6*t);
+disp('Laplace transform:');
+G(s)=laplace(g(t))
+
+%%
+% Example 9.3. Inverse Laplace Transform
+% 3*y'''(t) + 2*y''(t) + 3*y();
+% Initial condition: y(0)=0;y'(0)=0;y''(0)=;
+clc;clear;
+
+syms s Y
+% Initial condition
+y0=0;dy0=0;dy20=1;
+Y1=@(s) s*Y-y0;
+Y2=@(s) s*Y-s^2*y0-dy0;
+Y3=@(s) s^3*Y-s^2*y0-s*dy0-dy20;
+
+% Differential equation
+eqn=3*Y3(s)+2*Y2(s) + 3*Y - laplace(1,s);
+solve(eqn,Y)
