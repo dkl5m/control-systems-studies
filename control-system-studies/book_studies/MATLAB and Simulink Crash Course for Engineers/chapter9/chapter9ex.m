2028bf8888622d77a81a864389ba15ab92c895cd
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-08-07 23:56:39 -0300

    Add 9.4 to 9.8

--- control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter9/chapter9ex.m
+++ control-system-studies/book_studies/MATLAB and Simulink Crash Course for Engineers/chapter9/chapter9ex.m
@@ -45,3 +45,48 @@ syms t s
 G=@(t) 6/((s-3)^2+36);
 disp('Inverse laplace transform:')
 g(t)=ilaplace(G(s))
+
+%%
+% Example 9.5. Partial fraction
+% Fraction: (2s+3)/(s^2+2s)
+% Higher degree of Numerator < Higher degree of Denominator
+clc;clear;
+
+syms t s
+N = [2 3];
+D = [1 2 0];
+disp('The residuals:')
+[r,p,k] = residue(N,D)
+Expan=@(s) r(1)/(s-p(1)+r(2)/(s-p(2)));
+disp('The partial fraction expansion:')
+disp(Expan(s))
+
+%%
+% Example 9.6. Partial fraction - 2
+% Fraction: (2s^2+4s+1)/(s^2+2s)
+% Higher degree of Numerator = Higher degree of Denominator
+clc;clear;
+
+syms t s
+N = [2 4 1];
+D = [1 2 0];
+disp('The residuals:')
+[r,p,k] = residue(N,D)
+Expan=@(s) r(1)/(s-p(1)+r(2)/(s-p(2)))+k;
+disp('The partial fraction expansion:')
+disp(Expan(s))
+
+%%
+% Example 9.7. Partial fraction - 4
+% Fraction: (2s^3+4s^2+3s+1)/(1s^2+2s+1)
+% Higher degree of Numerator = Higher degree of Denominator
+clc;clear;
+
+syms t s
+N = [2 4 3 3];
+D = [1 2 1];
+disp('The residuals:')
+[r,p,k] = residue(N,D)
+Expan=@(s) r(1)/(s-p(1)+r(2)/(s-p(2)))+(s*k(1)+k(2));
+disp('The partial fraction expansion:')
+disp(Expan(s))
