0d35fba30f0c620832840a00c3b8de0186d15917
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-08-09 23:20:32 -0300

    add analog control

---
+++ control-system-studies/classes/analog control - aguirre/dominanciaModal.m
@@ -0,0 +1,23 @@
+% Dominancia modal 1
+close all; clear
+
+j = sqrt(-1);
+% 2 polos em s = -0.7 +- 1j
+den = conv([1 0.7+1i],[1 0.7-1i]);
+% incluir polo real em s = -2
+den = conv(den,[1 2]);
+% zero em s = -3;
+num = [1 3];
+
+% ganho CC
+Kcc = num(end)/den(end);
+
+% analise
+[r,p,k] = residue(num,den);
+% modo oscilatorio
+[numos,denos] = residue(r(2:3),p(2:3),k);
+% modo exponencial
+[numex,denex] = residue(r(1),p(1),k);
+
+% vetor de tempo
+t = 0:0.1:8;
