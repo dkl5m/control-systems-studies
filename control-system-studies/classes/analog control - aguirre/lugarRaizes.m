cd6b6d2925ef27c88f2de258870b2ec170f315eb
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-08-15 23:53:03 -0300

    add ex lugar raizes

--- control-system-studies/classes/analog control - aguirre/lugarRaizes.m
+++ control-system-studies/classes/analog control - aguirre/lugarRaizes.m
@@ -1,3 +1,27 @@
+% parametro
+K = [0 logspace(-2,2,29)];
+% posicao do zero
+z = -3;
+% inicializacao matriz que tera as raizes
+r = zeros(3,length(K));
+% traca os eixos
+plot([z 1],[0 0],'k',[0 0],[-10 10],'k')
+axis([z 1 -10 10]);
+hold on
+
+for j=1:length(K)
+    % polinomio em funcao de K
+    pol = [1 3 2+K(j) -z*K(j)];
+    % raizes do polinomio
+    r(:,j)=roots(pol)
+    % mostra as raizes para cada valor do K
+    % space para avancar
+    plot(real(r(:,j)),imag(r(:,j)),'b.')
+    pause
+end
+
+hold off
+
 %% Lugar das raizes
 % Ex1
 close all
