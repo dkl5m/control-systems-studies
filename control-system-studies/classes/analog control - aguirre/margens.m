01ffb3449afad70a95fab067f4552bf96340ccc4
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-08-20 22:12:28 -0300

    add ex2 in margens

--- control-system-studies/classes/analog control - aguirre/margens.m
+++ control-system-studies/classes/analog control - aguirre/margens.m
@@ -1,8 +1,10 @@
+% Margens de ganho e de fase
+% Ex.1
+close all; clear;
+
 % funcao transferencia malha
-% numerador
-num = 1;
-% denominador
-den = poly([-1 -1 0]);
+num = 1; % numerador
+den = poly([-1 -1 0]); % denominador
 % funcao transferencia
 sys = tf(num, den)
 
@@ -16,3 +18,25 @@ axis([-1.5 0.5 -1 1])
 
 % determinar margens e frequencias
 [mg,mf,wmg,wmf] = margin(sys)
+
+%% Ex.2 - Usando Bode
+close all; clear;
+
+% funcao transferencia malha
+num = 1;
+den = conv([1 1],[1 1]); % 2 polos em s=-1
+den = conv(den,[1 0]); % acrescimo polo em s=0
+GH = tf(num,den)
+
+% diagrama polar
+figure(1)
+nyquist(GH)
+axis('equal')
+axis([-1.5 0.5 -1 1])
+
+% diagrama Bode
+figure(2)
+bode(GH)
+grid
+
+margin(GH)
