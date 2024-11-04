76007253ba2b2fc6f27d62d6bed933a7abd78d10
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-11-03 23:32:46 -0300

    add part 2

--- CS_classes/dead time process introduction/ex1.m
+++ CS_classes/dead time process introduction/ex1.m
@@ -25,8 +25,8 @@ d2 = sqrt((sigma^2)+(c2^2));
 dbeta = sqrt((sigma^2)+(c2+cbeta)^2);
 d3 = sqrt((sigma^2)+(c2+cbeta+c3)^2);
 
-%K = (d1*d2*d3)/dbeta;
-K = 3
+K = (d1*d2*d3)/dbeta;
+% K = 3
 numC =[1 z];
 denC = [1 0];
 G = tf(num,den)
@@ -41,3 +41,37 @@ figure
 step(C)
 figure
 step(GC)
+
+%%
+clear; close; clc;
+
+num = 2;
+den = conv([1 (1+e)],[1 3]);
+
+MP = 0.05;              % max overshoot
+zeta = 0.7;             % damping ratio
+sigma = 0.75;            
+t5 = 3/sigma;           % tempo acomodacao 5%
+theta = acosd(zeta);
+
+c1 = sigma;
+c2 = 1 - sigma;
+c3 = 3 - sigma;
+alpha1 = 180 - theta;
+alpha2 = atand(sigma/c2);
+alpha3 = atand(sigma/c3);
+beta = 180 - (alpha1+alpha2+alpha3);
+o = tan(beta)*sigma;
+cbeta = sigma/tand(beta);
+z = 1 + o;
+
+d1 = sqrt((sigma^2)+(c1^2));
+d2 = sqrt((sigma^2)+(c2^2));
+dbeta = sqrt((sigma^2)+(c2+cbeta)^2);
+d3 = sqrt((sigma^2)+(c2+cbeta+c3)^2);
+
+% K = (d1*d2*d3)/dbeta;
+% K = 3
+K = 2 + 2*e
+numC =[1 z];
+denC = [1 0];
