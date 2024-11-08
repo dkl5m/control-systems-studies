82956e744017d8579808368ab17b669f7b140930
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-11-07 23:54:32 -0300

    add ex1

--- CS_classes/dynamic systems control/practices/1/lab1.m
+++ CS_classes/dynamic systems control/practices/1/lab1.m
@@ -1,3 +1,15 @@
+M=1;
+b=1;
+k=1;
+
+syms s U;
+
+A = [M*s^2+b*s+k];
+B = [U];
+
+x = linsolve(A,B)
+%%
+
 M1=1;
 M2=1;
 b1=1;
@@ -7,7 +19,7 @@ k2=1;
 
 syms s U1 U2;
 
-A = [M1*s^2+(b1-b2)*s+(k1-k2), b2*s+k2; -(b2*s+k2), M2*s^2+b2*s+k2];
-B = [U1; U2];
+A1 = [M1*s^2+(b1+b2)*s+(k1+k2), -(b2*s+k2); -(b2*s+k2), M2*s^2+b2*s+k2];
+B1 = [U1; U2];
 
-x_s = linsolve(A,B)
+x_s = linsolve(A1,B1)
