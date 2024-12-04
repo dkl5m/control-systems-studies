4c0ab0db700e23e38da31eb0fdfad1302dc79a5c
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-12-04 12:03:53 -0300

    add group 1c n 2b class 3

---
+++ CS_classes/dynamic systems control/practices/3/group_1C/practical_class_3_1c.m
@@ -0,0 +1,26 @@
+clear; close all; clc;
+
+% variaveis
+R = 10;
+L = 5;
+C = 5e-3;
+
+A = [0, -1/L; 1/C, -1/(R*C)];
+B = [1/L; 0];
+c = [0, 1/R];
+D = 0;
+c2 = [0 -1];
+D2 = 1;
+
+sys = ss(A,B,c,D)
+% step(sys)
+% [num,den] = ss2tf(A,B,c,D);
+% sys2 = tf(num,den)
+% step(sys2)
+% step(sys,sys2)
+% legend('sys','sys2')
+
+% [num2,den2] = ss2tf(A,B,c2,D2);
+% sys3 = tf(num2,den2)
+% step(sys3)
+
