376d26f20a9edd8befd5f905bd6b790af88d30f9
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-12-11 23:57:42 -0300

    add class 6

---
+++ CS_classes/dead time process introduction - ufc/class_6/class_6.m
@@ -0,0 +1,22 @@
+clear; close all; clc;
+
+% G(s) = 2/(3s + 1);
+% C = k*(s+z)/s;
+% Mp = 0.05; ts = 4;
+
+nump = 2;                           % plant numerator
+denp = [3 1];                       % plant denominator
+h = 6.5;                            % transport delay
+G = tf(nump,denp);                  % plant tf
+Gd = tf(nump,denp,'InputDelay',h);  % delayed plant tf
+% figure(1), margin(G);
+% figure(2), margin(Gs);
+
+% By rootlocus, z = 0.9839
+z = 0.9839;                         % controller zero
+% By |C*G| = 1, k = 5.3360
+k = 5.3360;                         % controller gain
+s = tf('s');                        % Laplace operator
+C = k*(s+z)/s;                      % PD controller
+Gc = C*G;                           % controlled plant
+
