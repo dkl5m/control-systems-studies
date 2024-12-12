ed5f061f5e947790dbd21a93588440e18feafebc
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-12-11 23:56:35 -0300

    add README with the classes descriptions

---
+++ CS_classes/dead time process introduction - ufc/README.txt
@@ -0,0 +1,9 @@
+This file contains what was made in each class.
+
+class_1 = Error's study
+class_2 = Smith Predictor (Continuous)
+class_3 = Arstein Predictor (Continuous)
+class_4 = Filtered Smith Predictor (Continuous)
+class_5 = Arstein Predictor (Discrete)
+class_6 = Filtered Smith Predictor (Discrete)
+class_7 = Simplified Filtered Smith Predictor (Discrete)
--- CS_classes/dead time process introduction - ufc/class_4/class_4.m
+++ CS_classes/dead time process introduction - ufc/class_4/class_4.m
@@ -8,7 +8,7 @@ nump = 2;                           % plant numerator
 denp = [3 1];                       % plant denominator
 h = 6.5;                            % transport delay
 G = tf(nump,denp);                  % plant tf
-Gs = tf(nump,denp,'InputDelay',h);  % delayed plant tf
+Gd = tf(nump,denp,'InputDelay',h);  % delayed plant tf
 % figure(1), margin(G);
 % figure(2), margin(Gs);
 
@@ -18,5 +18,5 @@ z = 0.9839;                         % controller zero
 k = 5.3360;                         % controller gain
 s = tf('s');                        % Laplace operator
 C = k*(s+z)/s;                      % PD controller
-Gc = C*G;
+Gc = C*G;                           % controlled plant
 
