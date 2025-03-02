a0618fbdddcdd9d74d12fd665cd27e39e73eff8d
dkl5m <141453881+dkl5m@users.noreply.github.com>
2025-03-01 23:59:43 -0300

    Update homework2.m

--- CS_classes/dead_time_process_introduction-ufc/homework2/homework2.m
+++ CS_classes/dead_time_process_introduction-ufc/homework2/homework2.m
@@ -3,8 +3,17 @@
 clear; close all; clc;
 
 % Plant variables
-nump = [0 2];                       % plant numerator
-denp = [3 1];                       % plant denominator
-h = 6.5;                            % transport delay
+nump = [-1 1];                      % plant numerator
+denp = [6 5 1];                     % plant denominator
+h = 1;                              % plant transport delay
 G = tf(nump,denp);                  % plant tf
 Gd = tf(nump,denp,'InputDelay',h);  % delayed plant tf
+
+% Uncertainty
+wn = 20; csi = 0.25;
+numu = wn^2;                        % uncertainty numerator
+denu = [1 2*csi*wn wn^2];           % uncertainty denominator
+hu = 0.25;                          % uncertainty transport delay
+U = tf(numu,denu);                  % uncertainty tf
+Ud = tf(numu,denu,'InputDelay',hu); % delayed uncertainty tf
+
