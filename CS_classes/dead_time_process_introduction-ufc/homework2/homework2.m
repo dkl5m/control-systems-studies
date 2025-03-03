f0f4292e65e6b9f8077043ab35470cf0623f6068
dkl5m <141453881+dkl5m@users.noreply.github.com>
2025-03-02 23:50:30 -0300

    add uncertainty and some info about the code

--- CS_classes/dead_time_process_introduction-ufc/homework2/homework2.m
+++ CS_classes/dead_time_process_introduction-ufc/homework2/homework2.m
@@ -1,19 +1,38 @@
-% homework1
-% specs: G(s) = 2/(3s + 1); C = k*(s+z)/s; Mp = 0.05; ts = 4;
+% homework2
 clear; close all; clc;
 
-% Plant variables
-nump = [-1 1];                      % plant numerator
-denp = [6 5 1];                     % plant denominator
-h = 1;                              % plant transport delay
-G = tf(nump,denp);                  % plant tf
-Gd = tf(nump,denp,'InputDelay',h);  % delayed plant tf
+% Replicate:
+% Simplified filtered Smith predictor for high-order dead-time processes
 
-% Uncertainty
-wn = 20; csi = 0.25;
-numu = wn^2;                        % uncertainty numerator
-denu = [1 2*csi*wn wn^2];           % uncertainty denominator
-hu = 0.25;                          % uncertainty transport delay
-U = tf(numu,denu);                  % uncertainty tf
-Ud = tf(numu,denu,'InputDelay',hu); % delayed uncertainty tf
+% Ex1
+% Stable dead-time process model
+% P(s) = (−s + 1)*exp(-s)/(3s + 1)(2s + 1);
+% Model uncertainty
+% Pr(s) = P(s) * wn^2*exp(-0.25*s)/(s^2+2*csi*wn*s+wn^2)
+wn = 20; csi = 0.25; % 25% dead-time uncertainty
+% Multiplicative uncertainty
+dP = (wn^2*exp(-0.25*s)/(s^2+2*csi*wn*s+wn^2))-1;
+Ts = 0.1; % sampling time
+% P(z) = (-0.0152*(z-1.1054)/((z-0.9672)*(z-0.9512)))*z^(-10);
 
+
+
+
+
+
+
+% % Plant variables
+% nump = [-1 1];                      % plant numerator
+% denp = [6 5 1];                     % plant denominator
+% h = 1;                              % plant transport delay
+% G = tf(nump,denp);                  % plant tf
+% Gd = tf(nump,denp,'InputDelay',h);  % delayed plant tf
+% 
+% % Uncertainty
+% wn = 20; csi = 0.25;
+% numu = wn^2;                        % uncertainty numerator
+% denu = [1 2*csi*wn wn^2];           % uncertainty denominator
+% hu = 0.25;                          % uncertainty transport delay
+% U = tf(numu,denu);                  % uncertainty tf
+% Ud = tf(numu,denu,'InputDelay',hu); % delayed uncertainty tf
+% 
